d84cad18ab10dc7a6cff965b524da7b9eaaacff3
Nattapat Sittiya <sittiya456@gmail.com>
2026-03-31 13:56:02 +0700

    change to astro flamework

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://sittiya456.github.io',
+  base: 'my-webpage',
+});
